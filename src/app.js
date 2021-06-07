101ae4817087db53e6de6785d3b23dad769f9d0f
Ernesto Velasco <94ernesto17@gmail.com>
2021-06-07 11:51:41 -0500

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const compression = require('compression')
+var minify = require('express-minify')
+
+const app = express()
+
+// config server
+app.use(compression())
+app.use(minify())
+app.use(express.static('public', {maxAge: 31557600}))
+
+// set routes to listen
+app.get('/', (req, res) => res.send('Weather App.'))
+app.get('/about', (req, res) => res.send('About'))
+app.get('/help', (req, res) => res.send('Help'))
+
+// config PORT
+const PORT = process.env.PORT || 3000
+
+// listening
+app.listen(PORT, () => console.log('Server running on port', PORT))
